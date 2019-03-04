871d6064fd2b233565739a2ba79e1979bbfacce0
Tobias Lang <t.lang@i6m.de>
2019-03-04 15:57:59 +0100

    Update Profile.js

--- Profile.js
+++ Profile.js
@@ -1,18 +1,18 @@
-const startX = Math.cos(2 * Math.PI * cumulativePercent);
-    const startY = Math.sin(2 * Math.PI * cumulativePercent);
+var startX = Math.cos(2 * Math.PI * cumulativePercent);
+    var startY = Math.sin(2 * Math.PI * cumulativePercent);
 
-    const slicePercent = value;
+    var slicePercent = value;
 
     cumulativePercent += slicePercent;
 
-    const endX = Math.cos(2 * Math.PI * cumulativePercent);
-    const endY = Math.sin(2 * Math.PI * cumulativePercent);
+    var endX = Math.cos(2 * Math.PI * cumulativePercent);
+    var endY = Math.sin(2 * Math.PI * cumulativePercent);
 
     // if the slice is more than 50%, take the large arc (the long way around)
-    const largeArcFlag = slicePercent > 0.5 ? 1 : 0;
+    var largeArcFlag = slicePercent > 0.5 ? 1 : 0;
 
     // create an array and join it just for code readability
-    const pathData = [
+    var pathData = [
       `M ${startX} ${startY}`, // Move
       `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
       `L 0 0` // Line
